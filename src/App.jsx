a46bf5476a309c7b28de5261714883eefa0edf77
Xunaiet Faruk <xunaiet28@gmail.com>
2026-07-19 21:54:37 +0600

    react and talwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
    
-   <h1 className='bg-red-400'>hellow world </h1>
+   <h1 className='bg-red-400 text-amber-300'>hellow world </h1>
 
    </>
   )
